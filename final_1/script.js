60b77a429e42a1ff0d7ad1302fad0c66bb20dccf
Sreekar617 <hindi6476@gmail.com>
2023-10-18 11:57:23 -0500

    day idk of wpd

---
+++ final_1/script.js
@@ -0,0 +1,26 @@
+const text = document.querySelector(".bouncing-text");
+let x = 0;
+let y = 0;
+let xDirection = 1;
+let yDirection = 1;
+
+function animate() {
+    const maxX = 0.99*window.innerWidth - 0.99*text.clientWidth;
+    const maxY = 0.98*window.innerHeight - 0.99*text.clientHeight;
+
+    x += 5 * xDirection;
+    y += 5 * yDirection;
+
+    if (x < 0 || x > maxX) {
+        xDirection *= -1;
+    }
+
+    if (y < 0 || y > maxY) {
+        yDirection *= -1;
+    }
+
+    text.style.transform = `translate(${x}px, ${y}px)`;
+    requestAnimationFrame(animate);
+}
+
+animate();
