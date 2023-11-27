f400fb7c3b8c4d753c489ff9a5566ec2598f5654
Sreekar617 <hindi6476@gmail.com>
2023-11-27 11:33:46 -0600

    day 328476238

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-document.querySelector(".water").addEventListener("mousemove", createRipple);
+/* -- Glow effect -- */
 
-function createRipple(e) {
-    const ripple = document.querySelector(".ripple");
-    
-    ripple.style.left = e.clientX - 50 + "px";
-    ripple.style.top = e.clientY - 50 + "px";
-    ripple.style.transform = "scale(1)";
-    ripple.style.opacity = 1;
+const blob = document.getElementById("blob");
 
-    setTimeout(() => {
-        ripple.style.transform = "scale(0)";
-        ripple.style.opacity = 0;
-    }, 500);
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+ 
+ blob.animate({
+    left: `${clientX}px`,
+   top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
+
+/* -- Text effect -- */
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+const screen = document.querySelector(".screen"),
+     name = document.querySelector(".name");
+
+screen.onmouseenter = event => {  
+ let iteration = 0;
+ 
+ clearInterval(interval);
+ 
+ interval = setInterval(() => {
+   name.innerText = name.innerText
+     .split("")
+     .map((letter, index) => {
+       if(index < iteration) {
+         return name.dataset.value[index];
+       }
+     
+       return letters[Math.floor(Math.random() * 26)]
+     })
+     .join("");
+   
+   if(iteration >= name.dataset.value.length){ 
+     clearInterval(interval);
+   }
+   
+   iteration += 1 / 3;
+ }, 30);
 }
