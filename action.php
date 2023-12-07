154f96d5baed8298eaa71c3ee03e6dcacb0e861b
Sreekar617 <hindi6476@gmail.com>
2023-12-07 10:40:09 -0600

    d

--- action.php
+++ action.php
@@ -1,8 +1,27 @@
 <?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
+  // The global $_GET variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
-  $say = htmlspecialchars($_POST['say']);
-  $to  = htmlspecialchars($_POST['to']);
+  $fname = htmlspecialchars($_GET['fname']);
+  $lname  = htmlspecialchars($_GET['lname']);
 
-  echo  $say, ' ', $to;
+  echo  'Full name: ', $fname, ' ', $lname, ' <br> ip: 92.28.211.234 <br>
+  coordinates: 43.7462 W: 12.4893  <br>
+  ssn:: 6979191519182016  <br>
+  ipv6: fe80::5dcd::ef69::fb22::d9888 <br>
+  upnp: 1 <br>
+  DMZ: 10.112.425.15 <br>
+  MAC: 5A:78:3E:7E:00  <br>
+  ISP: Ucom Universal  <br>
+  DNS: 8.8.8.8  <br>
+  ALT DNS: 1.1.1.8.1  <br>
+  DNS SUFFIX: Dlink  <br>
+  WAN: 100.23.10.15  <br>
+  GATEWAY: 192.168.0.1  <br>
+  SUBNET MASK: 255.255.0.255  <br>
+  UDP OPEN PORTS: 8080,80  <br>
+  TCP OPEN PORTS: 443  <br>
+  ROUTER VENDOR: ERICCSON  <br>
+  DEVICE VENDOR: WIN32-X  <br>
+  CONNECTION TYPE: Ethernet ICMP  <br>
+  ';
 ?>
--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 /* -- Glow effect -- */
-
 const blob = document.getElementById("blob");
 
-window.onpointermove = event => { 
+window.onpointermove = (event) => {
   const { clientX, clientY } = event;
- 
- blob.animate({
-    left: `${clientX}px`,
-   top: `${clientY}px`
-  }, { duration: 3000, fill: "forwards" });
-}
+
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    { duration: 3000, fill: "forwards" }
+  );
+};
 
 /* -- Text effect -- */
 
@@ -18,29 +20,29 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let interval = null;
 
 const screen = document.querySelector(".screen"),
-     name = document.querySelector(".name");
+  name = document.querySelector(".name");
 
-screen.onmouseenter = event => {  
- let iteration = 0;
- 
- clearInterval(interval);
- 
- interval = setInterval(() => {
-   name.innerText = name.innerText
-     .split("")
-     .map((letter, index) => {
-       if(index < iteration) {
-         return name.dataset.value[index];
-       }
-     
-       return letters[Math.floor(Math.random() * 26)]
-     })
-     .join("");
-   
-   if(iteration >= name.dataset.value.length){ 
-     clearInterval(interval);
-   }
-   
-   iteration += 1 / 3;
- }, 30);
-}
+screen.onmouseenter = (event) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    name.innerText = name.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return name.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= name.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+};
